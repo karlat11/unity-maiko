a82a6fc0d791e8e7790e2f5fcd54b79c5e445f5a
Karolina Latka <enforcerka@gmail.com>
2020-08-19 20:29:45 +0100

    Adding name carousels

--- Maiko/Assets/Scripts/nameSelectionManager.cs
+++ Maiko/Assets/Scripts/nameSelectionManager.cs
@@ -1,9 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
 
 public class nameSelectionManager : MonoBehaviour
 {
@@ -43,6 +40,38 @@ public class nameSelectionManager : MonoBehaviour
 
         nameText.text = nameList[nameIdx];
         surnameText.text = surnameList[surnameIdx];
+
+        surnameNext.onClick.AddListener(delegate () {
+            surnameIdx = nextIdx(surnameIdx, surnameText, surnameList);
+        });
+
+        surnamePrev.onClick.AddListener(delegate () {
+            surnameIdx = prevIdx(surnameIdx, surnameText, surnameList);
+        });
+
+        nameNext.onClick.AddListener(delegate () {
+            nameIdx = nextIdx(nameIdx, nameText, nameList);
+        });
+
+        namePrev.onClick.AddListener(delegate () {
+            nameIdx = prevIdx(nameIdx, nameText, nameList);
+        });
     }
 
+    private int prevIdx(int currentIdx, TextMeshProUGUI text, string[] list)
+    {
+        if (currentIdx > 0) currentIdx--;
+        else currentIdx = list.Length - 1;
+        text.text = list[currentIdx];
+
+        return currentIdx;
+    }
+
+    private int nextIdx(int currentIdx, TextMeshProUGUI text, string[] list)
+    {
+        if (currentIdx < list.Length - 1) currentIdx++;
+        else currentIdx = 0;
+        text.text = list[currentIdx];
+        return currentIdx;
+    }
 }
