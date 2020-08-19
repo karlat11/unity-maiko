0a0f2d895b8949463fe8ed8701e414f542776168
Karolina Latka <enforcerka@gmail.com>
2020-08-19 20:48:40 +0100

    Adding UI manager

---
+++ Maiko/Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public Button homeBtn, nextBtn, backBtn, quitBtn;
+
+    private void Start()
+    {
+        homeBtn.onClick.AddListener(delegate () {
+            SceneManager.LoadScene(0);
+        });
+
+        nextBtn.onClick.AddListener(delegate () {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        });
+
+        backBtn.onClick.AddListener(delegate () {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        });
+
+        quitBtn.onClick.AddListener(delegate () {
+            Application.Quit();
+        });
+    }
+
+}
