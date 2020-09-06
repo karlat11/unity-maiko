65ede690b260135b68cb733bde731a61e19fbaca
Karolina Latka <enforcerka@gmail.com>
2020-09-06 01:22:05 +0100

    Lvl selection update

---
+++ Maiko/Assets/Scripts/briefingCreator.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class briefingCreator : MonoBehaviour
+{
+    [TextArea(2, 10)]
+    public string[] sentences;
+
+    private void Start()
+    {
+        
+    }
+}
--- Maiko/Assets/Scripts/levelSelectionManager.cs
+++ Maiko/Assets/Scripts/levelSelectionManager.cs
@@ -1,29 +1,58 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 using UnityEngine.UI;
 
 public class levelSelectionManager : MonoBehaviour
 {
-    public Button[] levelBtns;
     public GameObject btns;
+    public TextMeshProUGUI briefing;
 
     private int levelsUnlocked;
-    //private Button[] lvlBtns;
+    private Button[] levelBtns;
 
     private void Start()
     {
-        Button[] test = btns.GetComponentsInChildren<Button>();
-        //lvlBtns = test;
+        levelBtns = btns.GetComponentsInChildren<Button>();
+        levelsUnlocked = 3;
 
-        if (test != null && test.Length != 0)
+        if (levelBtns != null && levelBtns.Length != 0)
         {
-            foreach (Button btn in test)
+            int i = 0;
+            foreach (Button btn in levelBtns)
             {
-                btn.interactable = false;
-                //GameObject[] img = btn.GetComponentsInChildren<GameObject>();
-                //foreach(GameObject im in img) Debug.Log("Here: " + im);
+                bool unlocked = i < levelsUnlocked;
 
-                //img.enabled = true;
+                Image[] img = btn.GetComponentsInChildren<Image>();
+                foreach (Image child in img)
+                {
+                    if (child.tag == "Locked")
+                    {
+                        Image childImg = child.GetComponentInChildren<Image>();
+                        updateLvlBtn(unlocked, btn, childImg);
+                    }
+                }
+
+                briefingCreator bCreator = btn.GetComponentInChildren<briefingCreator>();
+
+                btn.onClick.AddListener(delegate () {
+                    changeSelectedLvl(btn, bCreator.sentences);
+                });
+
+                i++;
             }
         }
+    }
+
+    void updateLvlBtn(bool unlocked, Button btn, Image img)
+    {
+        btn.interactable = unlocked;
+        img.enabled = unlocked ? false : true;
+    }
+
+    void changeSelectedLvl(Button btn, string[] copy)
+    {
+        foreach (Button child in levelBtns) child.interactable = true;
+        btn.interactable = false;
+        briefing.text = copy[0];
     }
 }
