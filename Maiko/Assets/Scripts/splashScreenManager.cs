29e175843e7bd482f5737693aad3a7b2329cceff
Karolina Latka <enforcerka@gmail.com>
2020-09-19 23:50:15 +0100

    Adding cache clear for testing and builds

--- Maiko/Assets/Scripts/splashScreenManager.cs
+++ Maiko/Assets/Scripts/splashScreenManager.cs
@@ -5,5 +5,8 @@ public class splashScreenManager : MonoBehaviour
     void Start()
     {
         Time.timeScale = 1f;
+
+        //For test data clearing
+        /*PlayerPrefs.SetString("PlayerName", "");*/
     }
 }
