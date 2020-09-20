18a0ed3e30c55c42ff6d59be3b3ad4b88659ae50
Karolina Latka <enforcerka@gmail.com>
2020-09-20 02:05:55 +0100

    Adding cache clear feature to splash

---
+++ Maiko/Assets/Scripts/clearCache.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class clearCache : MonoBehaviour
+{
+    public GameObject popUp;
+
+    private Button btn;
+    private Button closePopUpBtn, continueBtn;
+
+    private void Awake()
+    {
+        btn = GetComponent<Button>();
+        Button[] children = btn.GetComponentsInChildren<Button>();
+        foreach (Button child in children)
+        {
+            if (child.name == "close") closePopUpBtn = child;
+            else if (child.name == "continue") continueBtn = child;
+        }
+
+        popUp.SetActive(false);
+    }
+
+    private void Start()
+    {
+        btn.onClick.AddListener(delegate () {
+            popUp.SetActive(true);
+        });
+
+        closePopUpBtn.onClick.AddListener(delegate () {
+            popUp.SetActive(false);
+        });
+
+        continueBtn.onClick.AddListener(delegate () {
+            ClearSavedCache();
+        });
+    }
+
+    void ClearSavedCache()
+    {
+        PlayerPrefs.SetString("PlayerName", "");
+        PlayerPrefs.SetString("PlayerSurname", "");
+        PlayerPrefs.SetString("PlayerHair", "");
+        PlayerPrefs.SetString("PlayerMakeup", "");
+        PlayerPrefs.SetString("PlayerDress", "");
+        PlayerPrefs.SetInt("lvlsUnlocked", 1);
+
+        for (int j = 4; j < 12; j++)
+        {
+            PlayerPrefs.SetString(System.IO.Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(j)) + "_completed", "");
+        }
+    }
+}
--- Maiko/Assets/Scripts/splashScreenManager.cs
+++ Maiko/Assets/Scripts/splashScreenManager.cs
@@ -5,8 +5,5 @@ public class splashScreenManager : MonoBehaviour
     void Start()
     {
         Time.timeScale = 1f;
-
-        //For test data clearing
-        /*PlayerPrefs.SetString("PlayerName", "");*/
     }
 }
